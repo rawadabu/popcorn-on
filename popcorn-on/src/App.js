8e624ea00930adfc590e49aba77b4475f57f9848
rawadabu <rawadabusaleh0@gmail.com>
2023-09-17 17:43:49 +0300

    Building a res

--- popcorn-on/src/App.js
+++ popcorn-on/src/App.js
@@ -71,7 +71,7 @@ export default function App() {
       </Main>
     </>
   );
-}
+} 
 
 function Navbar({ children }) {
   // Structual component
--- popcorn-on/src/StarRating.js
+++ popcorn-on/src/StarRating.js
@@ -1,3 +1,25 @@
-export default function StarRating() {
-  return <div>Hello</div>;
+const containerStyle = { display: "flex", alignItems: "center", gap: "16px" };
+
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const testStyle = {
+  lineHeight: "1",
+  magrin: "0",
+};
+
+export default function StarRating({ maxRating = 5 }) {
+  // Setting by default
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p style={testStyle}>{maxRating}</p>
+    </div>
+  );
 }
