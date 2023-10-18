7b4a087c8d295363dbe3c3c7cf266200080ac401
rawadabu <rawadabusaleh0@gmail.com>
2023-10-18 12:30:48 +0300

    src/App.js: implemented fetching data from OMDB API using useEffect

--- popcorn-on/src/App.js
+++ popcorn-on/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,9 +50,24 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "c611dd7d";
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+  const query = "interstellar";
+
+  useEffect(function () {
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    }
+    fetchMovies();
+  }, []);
+
   // Structual component
   return (
     <>
@@ -71,7 +86,7 @@ export default function App() {
       </Main>
     </>
   );
-} 
+}
 
 function Navbar({ children }) {
   // Structual component
